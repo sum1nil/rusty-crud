1187ef6de878ab5acc415ec954e73980d33d1526
sum1nil <jaxsup@aol.com>
2020-05-08 01:34:41 -0500

    first commit

---
+++ src/lib/lib.rs
@@ -0,0 +1,21 @@
+pub fn public_function() {
+    println!("called rary's `public_function()`");
+}
+
+fn private_function() {
+    println!("called rary's `private_function()`");
+}
+
+pub fn indirect_access() {
+    print!("called rary's `indirect_access()`, that\n> ");
+
+    private_function();
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
